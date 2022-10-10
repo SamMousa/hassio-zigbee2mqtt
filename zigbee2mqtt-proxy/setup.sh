235174d20e691b70ade43856eec216c2efdf8ad4
Sam Mousa <sam@mousa.nl>
2022-10-10 15:34:51 +0200

    fix: use pure tempio instead of buggy bashio

--- zigbee2mqtt-proxy/setup.sh
+++ zigbee2mqtt-proxy/setup.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/bashio
-CONFIG_PATH=/data/options.json
-SERVER=$(bashio::config 'server')
-
-echo $SERVER
-bashio::var.json \
-    server "$SERVER" \
-    | tempio \
-        -template /nginx.conf.gtpl \
-        -out /tmp/nginx.conf
+#!/bin/sh
+tempio -conf /data/options.json -template /nginx.conf.gtpl -out /tmp/nginx.conf
 nginx -T -c /tmp/nginx.conf
